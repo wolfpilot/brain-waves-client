371c584c794f010ac4e9703e40e98dc5cd494021
Razvan Negrea <rznart@gmail.com>
2025-01-04 16:55:41 +0100

    Update the Engine to clear the Canvas on every render

--- src/utils/canvas/core/Engine.canvas.ts
+++ src/utils/canvas/core/Engine.canvas.ts
@@ -1,5 +1,6 @@
 // Stores
 import { type EngineStore, useEngineStore } from "@stores/engine.stores"
+import { type CanvasStore, useCanvasStore } from "@stores/canvas.stores"
 
 export interface Engine {
   init: () => void
@@ -7,19 +8,34 @@ export interface Engine {
 
 class EngineImpl implements Engine {
   private engineStore: EngineStore
+  private canvasStore: CanvasStore
 
   constructor() {
     this.engineStore = useEngineStore()
+    this.canvasStore = useCanvasStore()
   }
 
-  private render() {
+  private _clear() {
+    if (!this.canvasStore.ctx || !this.canvasStore.canvasSize) return
+
+    this.canvasStore.ctx.clearRect(
+      0,
+      0,
+      this.canvasStore.canvasSize.width,
+      this.canvasStore.canvasSize.height,
+    )
+  }
+
+  private _render() {
+    this._clear()
+
     this.engineStore.nodes.forEach((node) => node.draw())
 
-    window.requestAnimationFrame(() => this.render())
+    window.requestAnimationFrame(() => this._render())
   }
 
   public init() {
-    this.render()
+    this._render()
   }
 }
 
